cd1e60ff24bf531facd8e37454fd8a713e15219e
Moniesh Ravichandran <monieshravichandran@Monieshs-MacBook-Pro.local>
2023-02-01 22:49:50 +0530

    fixing spaces

--- pages/events.tsx
+++ pages/events.tsx
@@ -9,10 +9,10 @@ export default function Events() {
       <PageHead />
       <div className="flex flex-col items-center justify-center max-w-2xl min-h-screen p-4 m-auto text-white font-main">
         <CollegeLogo />
-        <h2 className="mb-8 text-4xl font-bold text-center lg:text-6xl font-title">
+        <h2 className="my-8 lg:text-4xl sm:text-3xl font-bold text-center lg:text-6xl font-title">
           Events
         </h2>
-        <h2 className="mb-8 text-4xl font-bold text-center">
+        <h2 className="mb-8 lg:text-4xl sm:text-2xl font-bold text-center">
             All events for SSN Instincts are powered by our different clubs, do check them out!
         </h2>
         <div className="p-4 text-xl shadow-2xl rounded-br-3xl grid lg:grid-cols-3 gap-10 w-[90vw] sm:grid-cols-2">
