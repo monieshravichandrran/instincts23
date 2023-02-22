7768d6446dad0d29ebb3a81b2a9c939f5d2c5827
R Akash <akashrangesh07@gmail.com>
2023-02-22 14:33:50 +0530

    Events comp UI change

--- components/events/index.tsx
+++ components/events/index.tsx
@@ -2,6 +2,13 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import { Fragment, useState } from "react";
 import Image from "next/image";
+import {
+  BsFillCalendarCheckFill,
+  BsFillClockFill,
+  BsFillPersonFill,
+} from "react-icons/bs";
+import { RiTeamFill } from "react-icons/ri";
+import { IoLocationSharp } from "react-icons/io5";
 
 type people = {
   name: string;
@@ -12,7 +19,6 @@ type ruleType = {
   rules: string[];
   ruleHead?: string;
 };
-
 
 type eventDetailsType = {
   eventNames: string;
@@ -31,12 +37,12 @@ type eventDetailsType = {
 type clubType = {
   name: string;
   imgsrc: string;
-}
+};
 
 type clubEvents = {
   events: eventDetailsType[];
   clubs: clubType[];
-}
+};
 
 type Children = {
   clubName: string;
@@ -47,34 +53,35 @@ export const Events = ({ clubName, eventDetails }: Children) => {
   const [selecteId, setSelectedId] = useState(0);
   return (
     <>
-    <div className="w-full mt-20 flex justify-center items-center">
-      {eventDetails.clubs.map((item)=>{
-        return(
-          <div className="mx-auto p-4 lg:p-8">
-          <Image
-            src={"/images/clubs/"+item.imgsrc}
-            alt={"elc"}
-            height={300}
-            width={300}
-          />
-          <h1 className="mt-3 font-title text-white align-center text-center text-2xl">{item.name}</h1>
-        </div>
-        )
-      })}
+      <div className="w-full mt-20 flex justify-center items-center">
+        {eventDetails.clubs.map((item) => {
+          return (
+            <div className="mx-auto p-4 lg:p-8">
+              <Image
+                src={"/images/clubs/" + item.imgsrc}
+                alt={"elc"}
+                height={300}
+                width={300}
+              />
+              <h1 className="mt-3 font-title text-white align-center text-center text-2xl">
+                {item.name}
+              </h1>
+            </div>
+          );
+        })}
       </div>
-      <div className="lg:ml-2 flex flex-wrap flex-row min-h-[70vh] pb-10 items-center justify-center ">
-        <div className="lg:flex-1 sm:w-[95%] md:w-[95%] mt-5 flex lg:flex-col  items-center sm:justify-center md:justify-center lg:justify-evenly  py-10  pr-1 pl-5 lg:min-h-[70vh] sm:min-h-fit md:min-h-fit shadow-2xl text-white sm:flex-row flex-wrap">
+      <div className="lg:ml-2 flex flex-wrap flex-row min-h-[100vh] pb-10 items-center justify-center ">
+        <div className="lg:flex-1 sm:w-[95%] md:w-[95%] mt-5 flex lg:flex-col  items-center sm:justify-center md:justify-center lg:justify-evenly  py-10  pr-1 pl-5 lg:min-h-[100vh] sm:min-h-fit md:min-h-fit shadow-2xl text-white sm:flex-row flex-wrap">
           {eventDetails.events.map((item, index) => (
             <div
               key={item.eventNames}
               onClick={() => {
                 setSelectedId(index);
-                const clubs = document.querySelector('.clubs');
-                if(!clubs)
-                  return;
-                clubs.classList.remove('clubitem');
-                window.requestAnimationFrame(function() {
-                  clubs.classList.add('clubitem');
+                const clubs = document.querySelector(".clubs");
+                if (!clubs) return;
+                clubs.classList.remove("clubitem");
+                window.requestAnimationFrame(function () {
+                  clubs.classList.add("clubitem");
                 });
               }}
               className="p-2 mb-2 bg-gray-800 rounded-lg font-title sm:mr-3 md:mr-3 md:text-xl hover:cursor-pointer hover:opacity-90"
@@ -118,51 +125,56 @@ export const Events = ({ clubName, eventDetails }: Children) => {
               <b className="mr-1 font-title">TBD</b>
             </p>
           )}
-          <div className="flex flex-wrap gap-5 mt-10">
-            <p className="text-lg text-justify text-white font-body md:text-xl">
-              <b className="mr-1 font-title">Date:</b>
-              {eventDetails.events[selecteId].date}
-            </p>
-            <p className="text-lg text-justify text-white font-body md:text-xl">
-              <b className="mr-1 font-title">Time:</b>{" "}
-              {eventDetails.events[selecteId].fromTime &&
-                eventDetails.events[selecteId].toTime ? (
-                <>
-                  {eventDetails.events[selecteId].fromTime} to{" "}
-                  {eventDetails.events[selecteId].toTime}
-                </>
-              ) : (
-                "TBD"
-              )}
-            </p>
-            <p className="text-lg text-justify text-white font-body md:text-xl">
-              <b className="mr-1 font-title">Venue:</b>{" "}
-              {eventDetails.events[selecteId].venue}
-            </p>
-            <p className="text-lg text-justify text-white font-body md:text-xl">
-              <b className="mr-1 font-title">Team Size:</b>{" "}
-              {eventDetails.events[selecteId].team_size
-                ? eventDetails.events[selecteId].team_size
-                : "TBD"}
-            </p>
-            <p className="text-lg text-justify text-white font-body md:text-xl">
-              <b className="mr-1 font-title">Type:</b>{" "}
-              {eventDetails.events[selecteId].type}
-            </p>
-          </div>
-          <div>
-            <h3 className="mt-10 text-lg text-white font-title">
+          <div className="flex flex-col items-start justify-around mt-5">
+            <div className="flex items-center justify-between lg:w-[23%] sm:w-[80%] md:w-[35%]">
+              <div className="rounded-3xl bg-white flex justify-around items-center p-3 text-black w-[100px]">
+                <BsFillCalendarCheckFill size={20} />
+                <p> {eventDetails.events[selecteId].date}</p>
+              </div>
+              <div className="rounded-3xl bg-white flex justify-around items-center p-3 text-black w-[100px]">
+                <BsFillClockFill size={20} />
+                <p>
+                  {eventDetails.events[selecteId].fromTime &&
+                  eventDetails.events[selecteId].toTime ? (
+                    <>
+                      {eventDetails.events[selecteId].fromTime} to{" "}
+                      {eventDetails.events[selecteId].toTime}
+                    </>
+                  ) : (
+                    "TBD"
+                  )}
+                </p>
+              </div>
+            </div>
+
+            <div className="rounded-3xl  flex justify-around items-center bg-white p-3 mt-3 text-black  w-[75px]">
+              <RiTeamFill size={20} />
+              <p>
+                {eventDetails.events[selecteId].team_size
+                  ? eventDetails.events[selecteId].team_size
+                  : "TBD"}
+              </p>
+            </div>
+
+            <div className="rounded-3xl bg-white flex justify-center items-center p-3 mt-3 text-black">
+              <IoLocationSharp size={20} />
+              <p> {eventDetails.events[selecteId].venue}</p>
+            </div>
+            <h3 className="mt-3 text-lg text-white font-title">
               Contact Numbers
             </h3>
             {eventDetails.events[selecteId].organizers?.length ? (
               <>
                 {eventDetails.events[selecteId].organizers?.map((item) => (
-                  <p
-                    key={item.name}
-                    className="text-lg text-justify text-white font-body md:text-xl"
-                  >
-                    {item.name} - {item.phone}
-                  </p>
+                  <div className="rounded-3xl bg-white flex justify-center items-center p-3 mt-3 text-black">
+                    <>
+                      <BsFillPersonFill size={20} />
+
+                      <p key={item.name}>
+                        {item.name} - {item.phone}
+                      </p>
+                    </>
+                  </div>
                 ))}
               </>
             ) : (
@@ -173,7 +185,7 @@ export const Events = ({ clubName, eventDetails }: Children) => {
           </div>
           {eventDetails.events[selecteId].ruleBook && (
             <a
-              className="mt-4 relative left-[55%] sm:left-[60%] md:left-[70%] lg:left-[80%]   w-full text-right m-4 text-white px-6 py-2 text-green-100 rounded bg-gradient-to-r from-red-800 via-yellow-600 to-yellow-500"
+              className="mt-4 relative left-[55%] sm:left-[60%] md:left-[70%] lg:left-[80%]  w-full text-right m-4  px-6 py-2 text-green-100 rounded bg-gradient-to-r from-red-800 via-yellow-600 to-yellow-500"
               href={eventDetails.events[selecteId].ruleBook}
             >
               Rule Book
