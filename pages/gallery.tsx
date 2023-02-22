cdc90a2d977738a77268c987e3cff50cb51deaca
monieshravichandrran <monieshravichandrran@gmail.com>
2023-02-23 01:40:16 +0530

    gallery added

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -8,12 +8,12 @@ export default function Events() {
   return (
     <>
       <PageHead />
-      <div className="flex flex-col items-center justify-center w-full min-h-screen p-2 md:p-4 m-auto text-white font-main">
+      <div className="flex flex-col items-center justify-center w-full min-h-screen p-2 md:p-4 m-auto text-white font-main gallerycont">
         <CollegeLogo />
         <script src="https://cdn.jsdelivr.net/npm/animejs@3.1.0/lib/anime.min.js" integrity="sha256-98Q574VkbV+PkxXCKSgL6jVq9mrVbS7uCdA+vt0sLS8=" crossOrigin="anonymous"></script>
         <script src="https://hammerjs.github.io/dist/hammer.js"></script>
-        <div className="min-h-screen lg:w-[98%] sm:w-full flex flex-col items-center justify-center">
-        <div className="slider" data-state="0">
+        <div className="min-h-screen lg:w-[98%] sm:w-full flex flex-col items-end justify-center">
+        <div className="slider w-[98vw] md:w-[95vw]" data-state="0">
 
           <div className="status">
             <div className="point" data-current="0"></div>
@@ -23,30 +23,45 @@ export default function Events() {
             <div className="stat" data-key="3"></div>
             <div className="stat" data-key="4"></div>
             <div className="stat" data-key="5"></div>
+            <div className="stat" data-key="6"></div>
+            <div className="stat" data-key="7"></div>
+            <div className="stat" data-key="8"></div>
+            <div className="stat" data-key="9"></div>
+            <div className="stat" data-key="10"></div>
+            <div className="stat" data-key="11"></div>
+            <div className="stat" data-key="12"></div>
+            <div className="stat" data-key="13"></div>
+            <div className="stat" data-key="14"></div>
+            <div className="stat" data-key="15"></div>
+            <div className="stat" data-key="16"></div>
+            <div className="stat" data-key="17"></div>
+            <div className="stat" data-key="18"></div>
+            <div className="stat" data-key="19"></div>
+            <div className="stat" data-key="20"></div>
           </div>
 
-          <div className="text">
-            <div className="current" data-key="0"></div>
-            <div data-key="1"></div>
-            <div data-key="2"></div>
-            <div data-key="3"></div>
-            <div data-key="4"></div>
-            <div data-key="5"></div>
-            <div data-key="6"></div>
-            <div data-key="7"></div>
-            <div data-key="8"></div>
-            <div data-key="9"></div>
-            <div data-key="10"></div>
-            <div data-key="11"></div>
-            <div data-key="12"></div>
-            <div data-key="13"></div>
-            <div data-key="14"></div>
-            <div data-key="15"></div>
-            <div data-key="16"></div>
-            <div data-key="17"></div>
-            <div data-key="18"></div>
-            <div data-key="19"></div>
-            <div data-key="20"></div>
+          <div className="text font-title text-[12vw] md:text-[6vw]">
+            <div className="current" data-key="0">Instincts</div>
+            <div data-key="1">Instincts</div>
+            <div data-key="2">Instincts</div>
+            <div data-key="3">Instincts</div>
+            <div data-key="4">Instincts</div>
+            <div data-key="5">Instincts</div>
+            <div data-key="6">Instincts</div>
+            <div data-key="7">Instincts</div>
+            <div data-key="8">Instincts</div>
+            <div data-key="9">Instincts</div>
+            <div data-key="10">Instincts</div>
+            <div data-key="11">Instincts</div>
+            <div data-key="12">Instincts</div>
+            <div data-key="13">Instincts</div>
+            <div data-key="14">Instincts</div>
+            <div data-key="15">Instincts</div>
+            <div data-key="16">Instincts</div>
+            <div data-key="17">Instincts</div>
+            <div data-key="18">Instincts</div>
+            <div data-key="19">Instincts</div>
+            <div data-key="20">Instincts</div>
           </div>
 
           <div className="image current" data-key={"0"} >
@@ -117,7 +132,6 @@ export default function Events() {
         </div>
         </div>
       </div>
-      <Footer />
       <Script src="slideIn.js" />
     </>
   );
